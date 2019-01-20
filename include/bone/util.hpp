33927eb6fd2a8bca2c7f153b5c35905c0c33c473
Baruch Burstein <bmburstein@gmail.com>
2019-01-20 08:53:28 +0200

    Added signed and generic min-size int templates

--- include/bone/util.hpp
+++ include/bone/util.hpp
@@ -23,4 +23,24 @@ using uint_min_size_t =
   std::uintmax_t
 >>>>;
 
+template <std::intmax_t MAX, std::intmax_t MIN=0>
+using sint_min_size_t =
+  std::conditional_t<
+    MAX <= std::numeric_limits<std::int8_t>::max() && MIN >= std::numeric_limits<std::int8_t>::min(),
+    std::int8_t,
+  std::conditional_t<
+    MAX <= std::numeric_limits<std::int16_t>::max() && MIN >= std::numeric_limits<std::int16_t>::min(),
+    std::int16_t,
+  std::conditional_t<
+    MAX <= std::numeric_limits<std::int32_t>::max() && MIN >= std::numeric_limits<std::int32_t>::min(),
+    std::int32_t,
+  std::conditional_t<
+    MAX <= std::numeric_limits<std::int64_t>::max() && MIN >= std::numeric_limits<std::int64_t>::min(),
+    std::int64_t,
+  std::uintmax_t
+>>>>;
+
+template <std::intmax_t MAX, std::intmax_t MIN=0>
+using int_min_size_t = std::conditional_t<MIN < 0, sint_min_size_t<MAX,MIN>, uint_min_size_t<MAX>>;
+
 }
--- tests/util_tests.cpp
+++ tests/util_tests.cpp
@@ -1,8 +1,42 @@
 #include "bone/util.hpp"
 
-static_assert(std::is_same_v<bone::uint_min_size_t<255>, std::uint8_t>);
-static_assert(std::is_same_v<bone::uint_min_size_t<256>, std::uint16_t>);
-static_assert(std::is_same_v<bone::uint_min_size_t<65535>, std::uint16_t>);
-static_assert(std::is_same_v<bone::uint_min_size_t<65536>, std::uint32_t>);
-static_assert(std::is_same_v<bone::uint_min_size_t<0xffffffff>, std::uint32_t>);
-static_assert(std::is_same_v<bone::uint_min_size_t<0x100000000>, std::uint64_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<UINT8_MAX>, std::uint8_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<UINT8_MAX+1>, std::uint16_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<UINT16_MAX>, std::uint16_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<UINT16_MAX+1>, std::uint32_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<UINT32_MAX>, std::uint32_t>);
+static_assert(std::is_same_v<bone::uint_min_size_t<uint64_t(UINT32_MAX)+1>, std::uint64_t>);
+
+static_assert(std::is_same_v<bone::sint_min_size_t<INT8_MAX>, std::int8_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT8_MAX+1>, std::int16_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT16_MAX>, std::int16_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT16_MAX+1>, std::int32_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT32_MAX>, std::int32_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<int64_t(INT32_MAX)+1>, std::int64_t>);
+
+static_assert(std::is_same_v<bone::sint_min_size_t<INT8_MAX,INT8_MIN>, std::int8_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT8_MAX,INT8_MIN-1>, std::int16_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT8_MAX+1,INT8_MIN>, std::int16_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT16_MAX,INT16_MIN>, std::int16_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT16_MAX,INT16_MIN-1>, std::int32_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT16_MAX+1,INT16_MIN>, std::int32_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT32_MAX,INT32_MIN>, std::int32_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<INT32_MAX,int64_t(INT32_MIN)-1>, std::int64_t>);
+static_assert(std::is_same_v<bone::sint_min_size_t<int64_t(INT32_MAX)+1,INT32_MIN>, std::int64_t>);
+
+static_assert(std::is_same_v<bone::int_min_size_t<UINT8_MAX>, std::uint8_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<UINT8_MAX+1>, std::uint16_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<UINT16_MAX>, std::uint16_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<UINT16_MAX+1>, std::uint32_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<UINT32_MAX>, std::uint32_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<uint64_t(UINT32_MAX)+1>, std::uint64_t>);
+
+static_assert(std::is_same_v<bone::int_min_size_t<INT8_MAX,INT8_MIN>, std::int8_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT8_MAX,INT8_MIN-1>, std::int16_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT8_MAX+1,INT8_MIN>, std::int16_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT16_MAX,INT16_MIN>, std::int16_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT16_MAX,INT16_MIN-1>, std::int32_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT16_MAX+1,INT16_MIN>, std::int32_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT32_MAX,INT32_MIN>, std::int32_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<INT32_MAX,int64_t(INT32_MIN)-1>, std::int64_t>);
+static_assert(std::is_same_v<bone::int_min_size_t<int64_t(INT32_MAX)+1,INT32_MIN>, std::int64_t>);
