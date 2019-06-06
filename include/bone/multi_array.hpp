43afcd155b60ba189a0204361b360c7151227e40
Baruch Burstein <bmburstein@gmail.com>
2019-06-06 17:37:37 +0300

    Add assignment and some  simple functions

--- include/bone/multi_array.hpp
+++ include/bone/multi_array.hpp
@@ -7,13 +7,32 @@
 #include <numeric>
 //#include <iostream>
 #include <stdexcept>
+#include <algorithm>
 
 namespace bone {
 
 template <typename T, std::size_t N>
 class frag {
 public:
-  frag(T* data, std::size_t* sizes) : data(data), sizes(sizes) {}
+  frag& operator=(frag const& rhs) {
+    if(std::equal(sizes, sizes+N, rhs.sizes)) {
+      std::copy(rhs.data, rhs.data+*sizes, data);
+    }
+    else {
+      throw std::out_of_range("assigning arrays of different length");
+    }
+    return *this;
+  }
+
+  frag& operator=(std::initializer_list<T> const& rhs) {
+    if(rhs.size() == *sizes) {
+      std::copy(rhs.begin(), rhs.end(), data);
+    }
+    else {
+      throw std::out_of_range("assigning arrays of different length");
+    }
+    return *this;
+  }
 
   template <typename... Args>
   decltype(auto) operator()(Args&&... args) {
@@ -28,7 +47,21 @@ public:
     return sizes[0] / sizes[1];
   }
 
-public:
+  std::size_t flat_size() {
+    return sizes[0];
+  }
+
+  T* flat_data() {
+    return data;
+  };
+
+protected:
+  frag(T* data, std::size_t* sizes) : data(data), sizes(sizes) {}
+  
+private:
+  template <typename U, std::size_t M>
+  friend class frag;
+
   template <typename... Args>
   auto getFrag(std::size_t i, Args&&... args) {
     return getFrag(i).getFrag(std::forward<Args>(args)...);
@@ -47,22 +80,27 @@ template <typename T>
 class frag<T, 0> {
 public:
   frag(T* data, std::size_t*) : data(*data) {}
+
+private:
+  template <typename U, std::size_t M>
+  friend class frag;
+
   T& get() { return data; }
 private:
   T& data;
 };
 
 template <typename T, std::size_t N>
-class multi_vector : public frag<T, N> {
+class multi_array : public frag<T, N> {
 public:
   template <typename... Args>
-  explicit multi_vector(Args... args) : multi_vector(std::vector<T>((... * args)), {std::size_t(args)...})
+  explicit multi_array(Args... args) : multi_array(std::vector<T>((... * args)), {std::size_t(args)...})
   {
     static_assert(sizeof...(args) == N);
   }
 
 private:
-  multi_vector(std::vector<T> d, std::vector<std::size_t> s)
+  multi_array(std::vector<T> d, std::vector<std::size_t> s)
     : frag<T,N>(d.data(), s.data())
     , data(std::move(d))
     , sizes(std::move(s))
--- tests/multi_array_tests.cpp
+++ tests/multi_array_tests.cpp
@@ -1,10 +1,10 @@
-#include "bone/multi_vector.hpp"
+#include "bone/multi_array.hpp"
 
 #include "catch.hpp"
 
-TEST_CASE("multi vector") {
+TEST_CASE("multi array") {
   SECTION("3d") {
-    bone::multi_vector<int, 3> a(4,2,3);
+    bone::multi_array<int, 3> a(4,2,3);
 
     SECTION("size") {
       REQUIRE(a.size() == 4);
@@ -12,7 +12,7 @@ TEST_CASE("multi vector") {
       REQUIRE(a(0,0).size() == 3);
     }
 
-    SECTION("assign") {
+    SECTION("assign value") {
       a(0,0,0) = 42;
       a(3)(1)(2) = 228;
       a[1](0,2) = 55;
@@ -24,10 +24,31 @@ TEST_CASE("multi vector") {
       REQUIRE(a[0][1][2] == 66);
       REQUIRE(a(1,1,1) == 77);
     }
+
+    SECTION("assign list") {
+      a(2,1) = {1,2,3};
+      // a[1] = {{4,5,6},{7,8,9}};
+      REQUIRE(a(2,1,0) == 1);
+      REQUIRE(a(2,1,1) == 2);
+      REQUIRE(a(2,1,2) == 3);
+    }
+
+    SECTION("assign fragment") {
+      a(0,1) = {1,2,3};
+      a(2)[1] = a[0][1];
+      REQUIRE(a(2,1,0) == 1);
+      REQUIRE(a(2,1,1) == 2);
+      REQUIRE(a(2,1,2) == 3);
+    }
+
+    SECTION("compare") {
+      bone::multi_array<int,3> b(4,2,3);
+
+    }
   }
 
   SECTION("1d") {
-    bone::multi_vector<char, 1> a(5);
+    bone::multi_array<char, 1> a(5);
 
     SECTION("size") {
       REQUIRE(a.size() == 5);
